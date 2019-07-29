7cd3b52fa560c8a204daaddb13a2db6b08c2c2de
jadonhansen <jadongavhansen@gmail.com>
2019-07-29 15:34:55 +0200

    ft_itoa_base

---
+++ level-04/ft_itoa_base.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhansen <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/29 14:09:25 by jhansen           #+#    #+#             */
+/*   Updated: 2019/07/29 15:34:29 by jhansen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_itoa_base(int value, int base)
+{
+	int		len;
+	int		temp;
+	char	*s;
+	char	*base_string = "0123456789ABCDEF";
+
+	len = 0;
+	temp = value;
+	if (value == 0)
+		return ("0");
+	while (temp)
+	{
+		temp = temp / base;
+		len++;
+	}
+	temp = value;
+	if (temp < 0)
+	{
+		if (base == 10)
+			len++;
+		temp = temp * -1;
+	}
+	s = (char *)malloc(sizeof(char) * len);
+	s[len] = '\0';
+	while (temp)
+	{
+		s[--len] = base_string[temp % base];
+		temp = temp / base;
+	}
+	if (value < 0 && base == 10)
+		s[0]= '-';
+	return (s);
+}
+
+//DO NOT INCLUDE ANYTHING BELOW
+
+#include <stdio.h>
+
+int		main(void)
+{
+	int num;
+	int	base;
+
+	base = 16;
+	num = -456;
+	printf("%s", ft_itoa_base(num, base));
+	return (0);
+}
